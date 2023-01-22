7fbe1902b1750174381f38d195ffe04f6dfcebde
chilanzi <davidchilanzijilliamdark@gmail.com>
2023-01-22 20:27:47 +0200

    updated the logic of the js script

--- Script.js
+++ Script.js
@@ -1,5 +1,7 @@
 //declare the rock paper scissors variables 
 var choice = ["Rock", "Paper", "Scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 // function for computer choice
 let getsComputerChoice = function() {
@@ -18,22 +20,37 @@ let PlayRound = function(playerChoice, ComputerChoice) {
         if (playerChoice.toLowerCase() === ComputerChoice.toLowerCase()) {
             message = "its a tie you both picked " + playerChoice;
         } else if (playerChoice.toLowerCase() === choice[0].toLowerCase() && ComputerChoice.toLowerCase() === choice[1].toLowerCase()) {
+            computerScore += 1;
             message = "You Lose! Paper beats Rock";
         } else if (playerChoice.toLowerCase() === choice[1].toLowerCase() && ComputerChoice.toLowerCase() === choice[2].toLowerCase()) {
+            computerScore += 1;
             message = "You Lose! scissors beats paper";
         } else if (playerChoice.toLowerCase() === choice[2].toLowerCase() && ComputerChoice.toLowerCase() === choice[0].toLowerCase()) {
+            computerScore += 1;
             message = "You Lose! rock beats scissors";
+        } else if (playerChoice.toLowerCase() !== choice[0].toLowerCase() || choice[1].toLowerCase() || choice[2].toLowerCase()) {
+            message = "wrong entery its rock paper scissors not " + playerChoice;
         } else {
-            message = "congratulations you win,you beat ai by playing " + playerChoice;
+            playerScore += 1;
+            message = "congratulations you win";
         }
 
         return message;
     }
     //playing 5 rounds 
 let game = function() {
-    for (let i = 0; i <= 5; i++) {
-        return PlayRound();
+    for (let i = 0; i <= 4; i++) {
+        console.log(PlayRound());
     }
 }
 
-console.log(game());
+let winner = function() {
+    game();
+    let msg;
+    if (playerScore > computerScore) {
+        msg = "you won the five round game computer won  " + computerScore + " rounds and you won " + playerScore + " rounds";
+    } else { msg = "you lose mate computer won " + computerScore + " rounds and you won " + playerScore + " rounds"; }
+    return msg;
+}
+
+console.log(winner());
