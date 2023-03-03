0d9ba712a57c5988e61ffa579b943bb9b999c519
chilanzi <davidchilanzijilliamdark@gmail.com>
2023-03-03 07:31:57 +0200

    ui updated

--- Script.js
+++ Script.js
@@ -1,56 +1,88 @@
-//declare the rock paper scissors variables 
+//declare the rock paper scissors variables
 var choice = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
 // function for computer choice
-let getsComputerChoice = function() {
-    let RandomChoice = Math.floor(Math.random() * 3);
-    return choice[RandomChoice];
-}
+let getsComputerChoice = function () {
+  let RandomChoice = Math.floor(Math.random() * 3);
+  return choice[RandomChoice];
+};
 
-// first rounnd 
-let PlayRound = function(playerChoice, ComputerChoice) {
+//Game logic function 
+let PlayRound = function (playerChoice) {
+  let ComputerChoice = getsComputerChoice();
+  let message;
 
-        playerChoice = prompt("pick rock paper or scissors");
-        ComputerChoice = getsComputerChoice();
-        let message;
+  //logic for the game rock beats scissors |scissors beats paper| paper beats rock
+  if (playerChoice.toLowerCase() === ComputerChoice.toLowerCase()) {
+    message = " stales " + playerChoice + " was played ";
+  } else if (
+    playerChoice.toLowerCase() === choice[0].toLowerCase() &&
+    ComputerChoice.toLowerCase() === choice[1].toLowerCase()
+  ) {
+    computerScore += 1;
+    message = "lost Round ! Paper beats Rock";
+  } else if (
+    playerChoice.toLowerCase() === choice[1].toLowerCase() &&
+    ComputerChoice.toLowerCase() === choice[2].toLowerCase()
+  ) {
+    computerScore += 1;
+    message = "lost round ! scissors beats paper";
+  } else if (
+    playerChoice.toLowerCase() === choice[2].toLowerCase() &&
+    ComputerChoice.toLowerCase() === choice[0].toLowerCase()
+  ) {
+    computerScore += 1;
+    message = "You Lose! rock beats scissors";
+  }  else {
+    playerScore += 1;
+    message = "You Won round";
+  }
 
-        //logic for the game rock beats scissors |scissors beats paper| paper beats rock
-        if (playerChoice.toLowerCase() === ComputerChoice.toLowerCase()) {
-            message = "its a tie you both picked " + playerChoice;
-        } else if (playerChoice.toLowerCase() === choice[0].toLowerCase() && ComputerChoice.toLowerCase() === choice[1].toLowerCase()) {
-            computerScore += 1;
-            message = "You Lose! Paper beats Rock";
-        } else if (playerChoice.toLowerCase() === choice[1].toLowerCase() && ComputerChoice.toLowerCase() === choice[2].toLowerCase()) {
-            computerScore += 1;
-            message = "You Lose! scissors beats paper";
-        } else if (playerChoice.toLowerCase() === choice[2].toLowerCase() && ComputerChoice.toLowerCase() === choice[0].toLowerCase()) {
-            computerScore += 1;
-            message = "You Lose! rock beats scissors";
-        } else if (playerChoice.toLowerCase() !== choice[0].toLowerCase() || choice[1].toLowerCase() || choice[2].toLowerCase()) {
-            message = "wrong entery its rock paper scissors not " + playerChoice;
-        } else {
-            playerScore += 1;
-            message = "congratulations you win";
-        }
+  return message;
+};
 
-        return message;
-    }
-    //playing 5 rounds 
-let game = function() {
-    for (let i = 0; i <= 4; i++) {
-        console.log(PlayRound());
-    }
-}
+let buttons = document.querySelectorAll("#buttons button, #buttons img");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let value = e.target.dataset.value;
 
-let winner = function() {
-    game();
-    let msg;
-    if (playerScore > computerScore) {
-        msg = "you won the five round game computer won  " + computerScore + " rounds and you won " + playerScore + " rounds";
-    } else { msg = "you lose mate computer won " + computerScore + " rounds and you won " + playerScore + " rounds"; }
-    return msg;
-}
+    // game logic 
+     let overall = document.getElementById("declaration");
+     let PScore = document.getElementById("player");
+     let CpuScore = document.getElementById("Computer");
+     let result = PlayRound(value);
+     let message = document.getElementById("winner");
 
-console.log(winner());
+     CpuScore.textContent = computerScore;
+     message.textContent = result;
+     PScore.textContent = playerScore;
+
+     let winner = function () {
+       msg = "";
+
+       if (playerScore == 5 && playerScore > computerScore) {
+         msg =
+           "you won game computer won  " +
+           computerScore +
+           " rounds and you won " +
+           playerScore +
+           " rounds";
+       } else if (computerScore >= 5 && computerScore > playerScore) {
+         msg =
+           "you lose computer won " +
+           computerScore +
+           " rounds and you won " +
+           playerScore +
+           " rounds";
+       }
+       return msg;
+     };
+
+     overall.textContent = winner();
+
+     console.log(result);
+  });
+});
+
